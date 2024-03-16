e404635e254c5458a7eac865594cb7b409caeae0
Jaden Cordova <jaden2004@berkeley.edu>
2024-03-16 12:10:07 -0700

    debug scripts

--- scripts/testScript.py
+++ scripts/testScript.py
@@ -1,19 +1,21 @@
 import os
 import subprocess
 import time
+from shlex import shlex
 
 pw = 'hello'
 pwAuth = f'sshpass -p {pw}'
+script = ''
 
 def filecounter():
     command = 'sudo python3 countCurrentTest.py'
-    return (int) subprocess.check_output(f'{pwAuth} ssh pi@raspberrypiednaregina.local {command}')
+    return int(subprocess.check_output(f'{pwAuth} ssh pi@raspberrypiednaregina.local {command}'))
 
 def start():
     pw = 'hello'
     init_fc = filecounter()
-    sshCommand = 'sudo python3 Sp24Test.py; exit'
-    command = shlex.split(f'{pwAuth} ssh pi@raspberrypiednaregina.local {command}')
+    sshCommand = f'sudo python3 {script}; exit'
+    command = shlex.split(f'{pwAuth} ssh pi@raspberrypiednaregina.local {sshCommand}')
     capture = subprocess.POpen(command)
     for i in range(1):    
         time.sleep(60)
@@ -21,6 +23,8 @@ def start():
             print('error')
             capture.kill()
             subprocess.run('python3 fileTransfer.py')
+    while capture.poll() is None:
+        time.sleep(1)
     subprocess.run('python3 fileTransfer.py')
 
 cwd = os.getcwd()
